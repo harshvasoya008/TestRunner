15f6715430944fd5dfc27c9165631f910016131d
harshvasoya008 <003harsh008@gmail.com>
2019-07-11 15:05:24 +0530

    basic cleanup

--- codes/rough.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char *p = 0;
-	int *q = 0;
-	return 0;
-}
--- codes/temp.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class temp{
-	public temp(){
-		rough2 r2 = new rough2();
-		r2.main(null);
-	}
-}
